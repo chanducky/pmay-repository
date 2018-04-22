8ed8538783468ff927064a992d4a9734567f4deb
chanducky <chandu.cky@gmail.com>
2018-04-22 21:46:06 +0530

    update file

--- src/main/resources/static/js/customise.js
+++ src/main/resources/static/js/customise.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
-
-	$(".myImg").click(function() {
+$(document).ready(function() {	
+	//$(".myImg").click(function() {
+	$(".sd-view").on( "click", ".myImg", function() {
 		$(".modal").css({
 			"display" : "block"
 		});
 		$(".modal-content").attr("src", this.src).width(this.naturalWidth).height(this.naturalHeight);
 	});
-	$(".close").click(function() {
+	//$(".close").click(function() {
+	$("body").on( "click", ".close", function() {
 		$(".modal").css({
 			"display" : "none"
 		});
